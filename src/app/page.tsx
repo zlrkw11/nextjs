55b08b52c4f4f2310b2e8f06c5e9f1c9d334b7a2
ZL <ray040406@gmail.com>
2024-09-26 23:57:00 +1200

    form event and prevent refreshing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 "use client";
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 export default function Home() {
   const [inputVal, setInputVal] = useState("");
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
   return (
     <div>
       <div>
         <h1>Enter your name</h1>
       </div>
-      <form>
-        <input type="text" placeholder="type your name..." />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="type your name..."
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
         <button type="submit"> Predict Data </button>
       </form>
     </div>
